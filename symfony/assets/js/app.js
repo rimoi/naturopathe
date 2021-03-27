1341d79e584567f861a86f49e6d9a0ad264751fa
rimoi <sidi.khalifa@live.fr>
2021-03-27 22:56:10 +0100

    fix homepage

--- symfony/assets/js/app.js
+++ symfony/assets/js/app.js
@@ -1,13 +1,26 @@
 import $ from 'jquery';
-require('bootstrap');
+global.$ = $;
+global.jQuery = $;
+// import {} from 'jqueryui';
+// window.$ = $;
+// require('bootstrap');
+// require('./ext/js1.js');
+// import {} from './ext/js_custom'
+// require('./ext/js2');
+// require('./ext/js3');
+// require('./ext/js4');
+// require('./ext/js5');
+// require('./ext/js6');
+// require('./ext/js7');
+// require('./ext/js8');
+// require('./ext/js9');
+// require('./ext/js10');
 import flashy from "./ext/flashy";
-import 'select2';
-import 'select2/dist/js/i18n/fr';
+// import 'select2';
+// import 'select2/dist/js/i18n/fr';
 window.flashy = flashy;
-import './scripts';
 
 import '../css/app.css';
-
 
 
 
